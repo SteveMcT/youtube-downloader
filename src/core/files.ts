610a36cf02ae607139d912f560f360ef2ee80f8a
SteveMcT <64359359+SteveMcT@users.noreply.github.com>
2022-02-08 21:01:17 +0100

    fixed typescript error

--- src/core/files.ts
+++ src/core/files.ts
@@ -1,9 +1,9 @@
-import fs from "fs";
+import { statSync } from "fs";
 
 export default class Files {
   static getFilesize(fileName: string): number {
     try {
-      var file = fs.statSync("/downloads/" + fileName);
+      var file = statSync("/downloads/" + fileName);
       return file.size;
     } catch (e) {
       return -1;
