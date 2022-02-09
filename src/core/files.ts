dcf2e087cc6ec6b2f65a1e92393f59d71a0c6d39
SteveMcT <64359359+SteveMcT@users.noreply.github.com>
2022-02-09 23:45:03 +0100

    fixed bugs, added ffmpeg

--- src/core/files.ts
+++ src/core/files.ts
@@ -3,7 +3,7 @@ import { statSync } from "fs";
 export default class Files {
   static getFilesize(fileName: string): number {
     try {
-      var file = statSync("/downloads/" + fileName);
+      var file = statSync("downloads/" + fileName);
       return file.size;
     } catch (e) {
       return -1;
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import chalk from "chalk";
+import { MultiBar } from "cli-progress";
 import cliSelect from "cli-select-2";
 import { readFileSync } from "fs";
 import Download from "./core/download";
 
 async function main() {
+  console.log(chalk.blue("Welcome to the YouTube Downloader!"));
+  console.log(chalk.blueBright("You need to have ffmpeg installed to use this tool."));
+  console.log(chalk.blueBright("On Windows you can install ffmpeg from https://ffmpeg.zeranoe.com/builds/"));
+  console.log(chalk.blueBright("On Linux you can install ffmpeg from https://ffmpeg.org/download.html"));
+  console.log(chalk.blueBright("On MacOS you can install ffmpeg from https://ffmpeg.zeranoe.com/builds/"));
+
   cliSelect({
     values: ["mp3", "mp4"],
     valueRenderer: (value, selected) => {
@@ -14,21 +21,34 @@ async function main() {
     },
   })
     .then(async (v) => {
+      // read file
       const file = readFileSync("./links.txt", "utf8");
       const links: string[] = file.split("\n");
 
+      // create console output
+      const bar = new MultiBar({
+        format: "Downloading [{bar}] {percentage}% | {value}/{total}",
+        barCompleteChar: "=",
+        barIncompleteChar: " ",
+        hideCursor: true,
+        clearOnComplete: false,
+        stopOnComplete: true,
+      });
+
       if (v.value == "mp3") {
-        for await (const link of links.map(async (e) => await Download.downloadMP3(e))) {
+        for (const link of links) {
+          Download.downloadMP3(bar, link);
         }
       }
 
       if (v.value == "mp4") {
-        for await (const link of links.map(async (e) => await Download.downloadMP4(e))) {
+        for (const link of links) {
+          Download.downloadMP4(bar, link);
         }
       }
     })
-    .catch((e) => {
-      chalk.red("An Error occurred");
+    .catch(() => {
+      console.log(chalk.red("An Error occurred"));
     });
 }
 
